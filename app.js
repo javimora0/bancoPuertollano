7a33b290d4dea8ff6584033f7dfb9cf7a9c0f006
JavierMorales <jmoralesr@protonmail.com>
2023-10-04 20:01:26 +0200

    problema al cargardatos

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function cargarDatos() {
+    document.getElementById("nombre").value = persona.nombre;
+    document.getElementById("apellido1").value = persona.apellido1;
+    document.getElementById("apellido2").value = persona.apellido2;
+    document.getElementById("nacionalidad").value = persona.nacionalidad;
+    document.getElementById("ibanInput").value = persona.iban;
+    document.getElementById("saldo").value = persona.saldo;
+    menu = document.getElementById("menu").innerHTML;
+  }
+  
+  function cargarCabecera(dest) {
+    document.getElementById(dest).innerHTML =
+      '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>';
+  }
+
 const botonRetirar = document.getElementById('botonRetirar')
 const botonIngresar = document.getElementById('botonRetirar')
 const saldoRetirado = document.getElementById('saldoRetirar')
@@ -31,18 +46,7 @@ botonIngresar.addEventListener("click", function () {
 
 })
 
-function cargarDatos() {
-  document.getElementById("nombre").value = persona.nombre;
-  document.getElementById("apellido1").value = persona.apellido1;
-  document.getElementById("apellido2").value = persona.apellido2;
-  document.getElementById("nacionalidad").value = persona.nacionalidad;
-  menu = document.getElementById("menu").innerHTML;
-}
 
-function cargarCabecera(dest) {
-  document.getElementById(dest).innerHTML =
-    '   <h1>BancoPuertollano</h1>    <ul>        <li><a href="index.html">Inicio</a></li>        <li><a href="infoCuenta.html">Informaci&#243;n Cuenta</a></li>             <li><a href="tarjetas.html">Tarjetas</a></li>    </ul>';
-}
 
 var persona = {
   nombre: "Francisco",
